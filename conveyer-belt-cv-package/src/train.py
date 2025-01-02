84893590f29099c4faa9ec0a76c29915eff3c3fd
sara-spreitzhofer <sara.spreitzhofer@tailored-apps.com>
2025-01-02 12:57:01 +0100

    Create labels

--- conveyer-belt-cv-package/src/train.py
+++ conveyer-belt-cv-package/src/train.py
@@ -1,14 +1,19 @@
-from ultralytics import YOLO
 import os
+import pandas as pd
 import shutil
 from sklearn.model_selection import train_test_split
+from ultralytics import YOLO
+
+source_folder = "../data/images/source"
+train_folder = "../data/images/train"
+val_folder = "../data/images/val"
+test_folder = "../data/images/test"
+csv_file = "../data/object_info.csv"
+train_labels_folder = "../data/labels/train"
+val_labels_folder = "../data/labels/val"
+
 
 def create_data():
-    source_folder = "../data/images/source"
-    train_folder = "../data/images/train"
-    val_folder = "../data/images/val"
-    test_folder = "../data/images/test"
-
     # Create destination folders if they don't exist
     for folder in [train_folder, val_folder, test_folder]:
         os.makedirs(folder, exist_ok=True)
@@ -33,13 +38,39 @@ def create_data():
     move_images(test_images, test_folder)
 
 
+def create_labels():
+    # Create labels folder if it doesn't exist
+    for folder in [train_labels_folder, val_labels_folder]:
+        os.makedirs(folder, exist_ok=True)
+
+    class_mapping = {"cube": 0, "cylinder": 1}
+    data = pd.read_csv(csv_file)
+
+    for image_name, group in data.groupby("image_name"):
+        if os.path.exists(os.path.join(train_folder, image_name)):
+            label_path = os.path.join(train_labels_folder, f"{os.path.splitext(image_name)[0]}.txt")
+        elif os.path.exists(os.path.join(val_folder, image_name)):
+            label_path = os.path.join(val_labels_folder, f"{os.path.splitext(image_name)[0]}.txt")
+        else:
+            print(f"Image {image_name} not found in train or val folders. Skipping...")
+            continue
+
+        with open(label_path, "w") as label_file:
+            for _, row in group.iterrows():
+                class_id = class_mapping[row["object_type"]]
+                # Convert ROS coordinates [-0.5, 0,5] to YOLO format [0, 1]
+                # TODO correct conversion
+                x_center, y_center, width, height = row["object_x"] + 0.5, row["object_y"] + 0.5, 0.08, 0.08
+                label_file.write(f"{class_id} {x_center} {y_center} {width} {height}\n")
+
+
 def train_model():
     model_path = os.path.join("..", "models", "yolov5s.pt")
     model = YOLO(model_path)
 
     model.train(
         data="data.yaml",
-        epochs=20,
+        epochs=5,
         imgsz=640,
         batch=16,
         workers=4,
@@ -50,5 +81,6 @@ def train_model():
     print(metrics)
 
 
-create_data()
-# train_model()
+# create_data()
+# create_labels()
+train_model()
